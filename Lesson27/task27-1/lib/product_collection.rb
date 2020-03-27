4fe575925b687dd83db32ca440ff769212b937c0
tee0zed <tee0zed@gmail.com>
2020-03-27 19:04:53 +0300

    27.03.2020

--- Lesson27/task27-1/lib/product_collection.rb
+++ Lesson27/task27-1/lib/product_collection.rb
@@ -1,34 +1,50 @@
 # encoding: utf-8
+require 'byebug'
 
 class ProductCollection
-  attr_accessor :all_products
-  # Словарь для связи классов и имен директорий.
-  PRODUCT_TYPES = {Book => 'books', Film => 'films', Disk => 'disks'}
+  attr_reader :sort_result, :books, :films, :disks
+  # Словарь для связи классов, массивов для инициализации экземпляра и имен директорий.
+  PRODUCT_TYPES =
+    {
+    Book => ['books', books = []],
+    Film => ['films', films = []],
+    Disk => ['disks', disks = []]
+    }
 
-  def initialize(products = [])
-    @all_products = products
+  def initialize(books, films, disks)
+    @books = books
+    @films = films
+    @disks = disks
+    @sort_result = []
   end
 
   def self.from_dir(path)
-    products = []
-
-    PRODUCT_TYPES.each_pair do |klass, dir|
+    PRODUCT_TYPES.each_pair do |klass, arr|
       # массив имен путей
-      paths = Dir[path + dir + "/*"]
+      paths = Dir[path + arr[0] + "/*"]
       # получить объекты класса, соответствующего имени директории в словаре PRODUCT_TYPES
       paths.each do |path|
-        products << klass.from_file(path)
+        obj = klass.from_file(path)
+        arr[1] << obj
       end
     end
     # создать экземпляр класса ProductCollection из массива всех полученных объектов
-    self.new(products)
+    self.new(PRODUCT_TYPES[Book][1], PRODUCT_TYPES[Film][1], PRODUCT_TYPES[Disk][1])
   end
 
   def to_str
     lines = []
+    n = 1
 
-    @all_products.each_with_index do |obj, indx|
-      lines << "#{indx + 1} #{obj.to_str}"
+    @sort_result.each do |obj|
+      # если не строка, а строки разделяют списки, то объект:
+      unless obj.is_a? String
+        lines << "#{n} #{obj.to_str}"
+        n += 1
+      else
+      # если строка, то пишем ее как сепаратор.
+        lines << "\n" + obj
+      end
     end
     lines
   end
@@ -37,53 +53,41 @@ class ProductCollection
     @all_products
   end
 
-  def sort!(products, params)
-    # если не передано ни одного аргумента:
-    params ||= {by: :type}
+  def sort!(params = {})
+    # если не задано никаких параметров:
+    params[:by] ||= :tit
+    params[:group?] ||= false
+    # массив для результатов сортировки
+    @sort_result = []
 
     if params[:group?]
-      disks.sort!(params)
+    # если сортируем по группам товаров, то передаем в метод sort каждый тип товара.
+      [@books, @disks, @films].each do |type|
+        sorting(params, type)
+      # вставим в массив строку с названием класса
+        @sort_result << "#{type.first.class}s"
+      # и отсортированный класс в этот же массив
+        @sort_result += type
+      end
+    # если по типу не сортируем, то берем массив всех товаров.
     else
-      products = @all_products
+      products = @books + @disks + @films
+      @sort_result += sorting(params, products)
     end
+    # для вызова по цепочке (пригодится?)
+    @sort_result
+  end
 
+  def sorting(params, obj)
     case params[:by]
-    when :type
-      products.sort_by! {|product| product.class.to_s}
     when :title
-      products.sort_by! {|product| product.title}
-    when :amount
-      products.sort_by! {|product| product.amount}
+      obj.sort_by! {|product| product.title}
     when :price
-      products.sort_by! {|product| product.price}
+      obj.sort_by! {|product| product.price}
+    when :amount
+      obj.sort_by! {|product| product.amount}
     end
-    products.reverse! if params[:ord] == :asc
-    self
-  end
-
-  private
-
-  def by_type
-    "Диски:
-    #{disks.sort!(params)}
-
-    Книги:
-    #{books.sort!(params)}
-
-    Фильмы:
-    #{films.sort!(params)}"
-  end
-
-  # массивы по типам объектов
-  def disks
-    @all_products.select { |product| product.is_a? Disk }
-  end
-
-  def books
-    @all_products.select { |product| product.is_a? Book }
-  end
-
-  def films
-    @all_products.select { |product| product.is_a? Film }
+    obj.reverse! if params[:asc]
+    obj
   end
 end
--- Lesson27/task27-1/main.rb
+++ Lesson27/task27-1/main.rb
@@ -5,11 +5,58 @@ require_relative 'lib/book'
 require_relative 'lib/film'
 require_relative 'lib/disk'
 require_relative 'lib/product_collection'
+require 'byebug'
 
 path = __dir__ + '/data/'
 
 collection = ProductCollection.from_dir(path)
+# метод принимает параметрами 3 символа - порядок(asc: true/false),
+# сортировка по (by: amount, price, title) и группировка по классам (group?: true/false)
+# метод может не принимать параметры, тогда группировка происходит по типу, названию и
+# в порядке возрастания.
+collection.sort!(group?: true, by: :title)
 
-collection.sort!(by: :price, group?: true)
+# получим массив товаров без строчек-сепараторов
+products_array = collection.sort_result.reject {|obj| obj.is_a? String}
 
-puts collection.to_str
+
+puts "Вот что у нас есть:"
+puts
+
+
+
+shopped = []
+
+input = nil
+
+until input == 0
+  puts "\n\nВ корзине:\n"
+
+  shopped.each do |item|
+    puts products_array[item-1].to_str
+  end
+
+  puts
+  puts collection.to_str
+  puts "\n*****\n0 - оплатить и выйти.
+  \nВыберите покупку"
+
+  input = gets.to_i
+  shopped << input
+end
+
+sum = 0
+
+puts "Вы купили:"
+shopped.each do |item|
+  sum += products_array[item-1].price
+
+  products_array[item-1].amount -= 1
+
+  puts products_array[item-1].to_str
+end
+
+puts "С вас: #{sum} рублей."
+
+
+
